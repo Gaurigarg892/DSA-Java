12d8bd80a51eacfc9b7de4399622d0f108d3fe38
Gauri garg <gaurigarg699@gmail.com>
2026-08-12 18:54:18 +0530

    ✅ Length of Longest Subarray With at Most K Frequency | Medium | Java | Runtime: 68 ms (50.38%) | Memory: 101.6 MB | 2026-08-12

---
+++ leetcode/medium/2958-length-of-longest-subarray-with-at-most-k-frequency/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int left =0; 
+        
+         int max_length = 0;
+         HashMap<Integer , Integer> map = new HashMap<>();
+
+         for(int right =0; right<nums.length ; right++){
+            map.put(nums[right] , (map.getOrDefault(nums[right] , 0)+1));
+            while(map.get(nums[right])>k){
+            map.put(nums[left] ,   map.get(nums[left])-1  );
+            left++;
+
+
+         }
+             max_length = Math.max(max_length , right-left+1);
+
+         }
+         
+       
+        return max_length;
+    }
+}
