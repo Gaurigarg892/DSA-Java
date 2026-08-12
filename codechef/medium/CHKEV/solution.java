8bd002e58ac0dacdf08106d6288c79c72dd1a06a
Gauri garg <gaurigarg699@gmail.com>
2026-08-12 20:21:11 +0530

    ✅ CHKEV | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/CHKEV/solution.java
+++ codechef/medium/CHKEV/solution.java
@@ -7,7 +7,25 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	 Scanner in = new Scanner(System.in);
-	 int 
+	 int L= in.nextInt();
+	 int R = in.nextInt();
+	 
+	 if(L == R){
+	     return no;
+	 }
+	 if(R-L-1 > 2){
+	     return yes;
+	 }
+	 
+	 else{
+	     if(L+1 % 2 == 0){
+	         return YES;
+	         
+	     }
+	     else {
+	         return no;
+	     }
+	 }
 
 	}
 }
