3dfd5e885be16146638fa7427ee244a5de6d00ef
Gauri garg <gaurigarg699@gmail.com>
2026-08-17 18:17:59 +0530

    ✅ Max Consecutive Ones III | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-17

---
+++ leetcode/medium/1004-max-consecutive-ones-iii/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int counter = 0;
+        int max = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                counter++;
+            }
+            while (counter > k) {
+
+                if (nums[left] == 0) {
+                    counter--;
+                }
+
+                left++;
+            }
+
+            
+            int windowSize = right - left + 1;
+            max = Math.max(max, windowSize);
+        }
+
+        return max;
+    }
+}
