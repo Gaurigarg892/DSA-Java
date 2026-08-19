aa9d0c1840c2d046ff687e842b1aa3f1b7df8482
Gauri garg <gaurigarg699@gmail.com>
2026-08-19 21:15:44 +0530

    ✅ MRSWAP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/MRSWAP/solution.java
+++ codechef/medium/MRSWAP/solution.java
@@ -6,7 +6,29 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	Scanner in = new Scanner(System.in);
+	int T = in.nextInt();
+	
+	
+	for(int i=1; i<=T; i++){
+	    int N= in.nextInt();
+	    int[] arr = new int[2 * N];
+	    
+	    for(int j=0; j< 2 * N ; j++){
+	        arr[j] = in.nextInt();
+	        
+	    }
+	    int sum=0;
+	    for(int a=0 ; a<N; a++){
+	       
+	        if(arr[a]<arr[2 * N - 1 - a]){
+	        arr[a] = arr[2 * N  - 1 - a];
+	        }
+	        sum+= arr[a];
+	        
+	    }
+	    System.out.println(sum);
+	}
 
 	}
 }
