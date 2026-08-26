c2176e53ff5c5a0ca164d30347b45633d0f5909d
Gauri garg <gaurigarg699@gmail.com>
2026-08-26 23:02:46 +0530

    ✅ POSHOP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/POSHOP/solution.java
+++ codechef/medium/POSHOP/solution.java
@@ -6,30 +6,45 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner in = new Scanner(System.in);
-		int T = in.nextInt();
-		for(int i= 0 ; i<T ; i++){
-		    int N = in.nextInt();
-		    int max_sum = 0;
-		    int sum =0;
-		    int[] arr = new int[N];
-		    
-		    for(int j=0; j<N ;j++){
-		        arr[j] = in.nextInt();
-		    }
-		    int count= 0;
-		    for(int a= 0; a<N; a++){
-		        max_sum = Math.max(max_sum, arr[a]);
-		        for(int b=a+1 ; b<N ; b++){
-		            if(arr[a]<=arr[b]){
-		                sum = arr[a]+arr[b];
-		                max_sum = Math.max(max_sum ,sum);
-		            }
-		            
-		        }
-		    }
-		    System.out.println(max_sum);
-		}
+	Scanner in = new Scanner(System.in);
+	int T= in.nextInt();
+	for(int i=0 ; i< T; i++){
+	     int max_sum = 0;
+	    int N =in.nextInt();
+	    int[] arr = new int[N];
+	    for(int j=0 ;j<N ;j++){
+	        arr[j] = in.nextInt();
+	        
+	    }
+	    
+	    
+	    int left = 0;
+	    int count= 0;
+	   
+	   int sum =0;
+	    for(int right = 0; right < N ; right++){
+	        
+	        while(arr[i] < sum){
+	            continue;
+	        }
+	        while(count < 2){
+	            sum += arr[right];
+	            count++;
+	            
+	          
+	        }
+	        while(count>2){
+	            sum -= arr[left];
+	            count--;
+	            left++;
+	            
+	            max_sum = Math.max(max_sum , sum);
+	        }
+	        
+	    }
+	    
+	}
+	System.out.println(max_sum);
 
 	}
 }
