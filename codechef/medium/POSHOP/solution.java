04bd18b6b2c43ebf0034a84f2eca5028e18b8b48
Gauri garg <gaurigarg699@gmail.com>
2026-08-26 21:13:04 +0530

    ✅ POSHOP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ codechef/medium/POSHOP/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner in = new Scanner(System.in);
+	int T= in.nextInt();
+	for(int i=0 ; i< T; i++){
+	    int N =in.nextInt();
+	    int[] arr = new int[N];
+	    for(int j=0 j<N ;j++){
+	        arr[j] = in.nextInt();
+	        
+	    }
+	    
+	    
+	    
+	    
+	}
+
+	}
+}
