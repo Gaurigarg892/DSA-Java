d8d1870985b51dc5570e4184d084c1ca1fd237cd
Gauri garg <gaurigarg699@gmail.com>
2026-08-26 21:31:24 +0530

    ✅ POSHOP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/POSHOP/solution.java
+++ codechef/medium/POSHOP/solution.java
@@ -9,17 +9,42 @@ class Codechef
 	Scanner in = new Scanner(System.in);
 	int T= in.nextInt();
 	for(int i=0 ; i< T; i++){
+	     int max_sum = 0;
 	    int N =in.nextInt();
 	    int[] arr = new int[N];
-	    for(int j=0 j<N ;j++){
+	    for(int j=0 ;j<N ;j++){
 	        arr[j] = in.nextInt();
 	        
 	    }
 	    
 	    
-	    
+	    int left = 0;
+	    int count= 0;
+	   
+	   int sum =0;
+	    for(int right = 0; right < N ; right++){
+	        
+	        while(arr[i] < sum){
+	            continue;
+	        }
+	        while(count < 2){
+	            sum += arr[right];
+	            count++;
+	            
+	          
+	        }
+	        while(count>2){
+	            sum -= arr[left];
+	            count--;
+	            left++;
+	            
+	            max_sum = Math.max(max_sum , sum);
+	        }
+	        
+	    }
 	    
 	}
+	System.out.println(max_sum);
 
 	}
 }
