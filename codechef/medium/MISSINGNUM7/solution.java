f7d54e4d7c6f0fffef074719b7f9059d0162f363
Gauri garg <gaurigarg699@gmail.com>
2026-09-02 21:28:23 +0530

    ✅ MISSINGNUM7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/MISSINGNUM7/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner in = new Scanner(System.in);
+	int S = in.nextInt();
+	System.out.println(10 -S);
+
+	}
+}
