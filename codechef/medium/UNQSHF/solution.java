f642a7dde8b37a84dc77c9393327eb6e1b3cf98d
Gauri garg <gaurigarg699@gmail.com>
2026-09-09 20:44:42 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/UNQSHF/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in = new Scanner(System.in);
+		int T=in.nextInt();
+		for(int t = 1 ; t <= T ; t++){
+		    int N =in.nextInt();
+		    String A = in.next();
+		    String B = in.next();
+		}
+
+	}
+}
