9208a49eb308c7ec663d048cfce2241ac6f753e2
Gauri garg <gaurigarg699@gmail.com>
2026-09-16 21:10:32 +0530

    ✅ REDBLUE7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/REDBLUE7/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner in = new Scanner(System.in);
+	int T =in.nextInt();
+	for(int t=1 ; t<= T ;t++){
+	    int N = in.nextInt();
+	    int[] A = new int[N];
+	    for(int i=0; i< N ;i++){
+	        A[i]= in.nextInt();
+	    }
+	    
+	    
+	}
+
+	}
+}
