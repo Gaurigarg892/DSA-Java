1195c13ed0d98ccce1fac08fea0896a141cb8768
Gauri garg <gaurigarg699@gmail.com>
2026-09-16 20:58:37 +0530

    ✅ FIXEDPTS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/FIXEDPTS/solution.java
+++ codechef/medium/FIXEDPTS/solution.java
@@ -6,7 +6,19 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	Scanner in =new Scanner(System.in);
+	int T= in.nextInt();
+	for(int t=1 ; t<=T ; t++){
+	    int N = in.nextInt();
+	    int K = in.nextInt();
+	    
+	    if(N-K > 1){
+	        System.out.println("Yes");
+	    }
+	    else{
+	        System.out.println("No");
+	    }
+	}
 
 	}
 }
